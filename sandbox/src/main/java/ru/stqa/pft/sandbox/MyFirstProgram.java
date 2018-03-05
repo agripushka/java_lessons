875b8c105bce413e54bfe767e73b86c803e744d9
Agripushka <abc-92@yandex.ru>
2018-03-05 21:33:18 +0300

    задание №2 пункт 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,14 +5,16 @@ public class MyFirstProgram {
 		Point p1 = new Point(10,15);
 		Point p2 = new Point(20,25);
 
-		print("Расстояние = " + distance(p1,p2));
+		/*print("Расстояние = " + distance(p1,p2));*/ /*пункт 3*/
+		print("Расстояние = " + Point.distance(p1,p2));
 	}
 	public static void print (String somebody)  {
 		System.out.println(somebody);
 	}
-	public static double distance(Point p1, Point p2) {
+	/*пункт 3*/
+	/*public static double distance(Point p1, Point p2) {
 		double distance = Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
 		return distance;
-	}
+	}*/
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,4 +7,10 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    /*4-й пункт*/
+    public static double distance(Point p1, Point p2) {
+        double distance = Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+        return distance;
+    }
 }
