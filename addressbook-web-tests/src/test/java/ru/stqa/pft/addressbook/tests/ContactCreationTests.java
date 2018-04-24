db4f427664c1db4be76012dd0ed1808daedd3711
agripushka <abc-92@yandex.ru>
2018-04-24 18:51:52 +0300

    Лекция 6.5.
    Загрузка тестовых данных из файла для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,12 +18,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        File photo = new File("src/test/resources/stru.png");
-        list.add(new Object[] {new ContactData().withFirstname("Имя1").withLastname("Фамилия1").withAddress("адрес").withEmail("test@test.ru").withMobilePhone("+99999999999").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withFirstname("Имя2").withLastname("Фамилия2").withAddress("адрес").withEmail("test@test.ru").withMobilePhone("+99999999999").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withFirstname("Имя3").withLastname("Фамилия3").withAddress("адрес").withEmail("test@test.ru").withMobilePhone("+99999999999")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.csv")));
+        String line = reader.readLine();
+        do {
+            String[] split = line.split(",");
+            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])
+                    .withAddress(split[2]).withEmail(split[3]).withMobilePhone(split[4])});
+            line = reader.readLine();
+        } while (line != null);
         return list.iterator();
     }
 
@@ -32,12 +36,13 @@ public class ContactCreationTests extends TestBase {
         File photo = new File("src/test/resources/stru1.jpg");
         Contacts before = app.contact().all();
         app.goTo().contactCreationPage();
-        app.contact().create(contact.withPhoto(photo), true);
+        app.contact().create(contact, true);
         app.goTo().homepage();
         assertThat(app.contact().count(), equalTo(before.size() +1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+
     }
 
 }
